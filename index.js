b87f486938ffc3030e3037118477c7ec7cadfe29
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-03 01:55:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = 'e26ebb0f26582930c9d38ed53b7956278cf4fc11'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
