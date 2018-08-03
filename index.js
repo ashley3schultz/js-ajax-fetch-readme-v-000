4f90613fba2e51901766697bb1fa9922725f8815
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-03 01:51:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much.";
 
-const token = 'YOUR_TOKEN_HERE'
+const token = 'e26ebb0f26582930c9d38ed53b7956278cf4fc11'
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
